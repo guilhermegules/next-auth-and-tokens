dde32a5d13778f0d6bc4dd8b02d6ceba5235c811
guilhermegules <guilhermegules@gmail.com>
2023-09-13 22:26:26 -0300

    feat: create token service

--- frontend/pages/auth-page-ssr.js
+++ frontend/pages/auth-page-ssr.js
@@ -1,14 +1,20 @@
+import { tokenService } from "../src/services/auth/token-service";
+
 function AuthPageSSR(props) {
   return (
     <div>
-      <h1>
-        Auth Page Server Side Render
-      </h1>
-      <pre>
-        {JSON.stringify(props, null, 2)}
-      </pre> 
+      <h1>Auth Page Server Side Render</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
-  )
+  );
 }
 
 export default AuthPageSSR;
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {
+      token: tokenService.get(ctx),
+    },
+  };
+}
--- frontend/src/services/auth/auth-service.js
+++ frontend/src/services/auth/auth-service.js
@@ -1,4 +1,5 @@
 import { httpClient } from "../../infra/http/http-client";
+import { tokenService } from "./token-service";
 
 export const authService = {
   async login(body) {
@@ -11,6 +12,8 @@ export const authService = {
     );
 
     if (!response.ok) throw new Error("Unknown error!");
+
+    tokenService.save(null, response.data.data.access_token);
 
     return response.data;
   },
